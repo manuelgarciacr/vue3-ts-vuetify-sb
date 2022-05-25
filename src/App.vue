378ec233b5d5347d0de7f1a26eed3d32fed6d605
Manuel García Criado <manuel.garcia.cr@hotmail.com>
2022-05-25 22:00:37 +0200

    Nav
     On branch main
     Changes to be committed:
    	modified:   README.md
    	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+    <v-app>
+        <v-main>
+            <nav>
+                <router-link to="/">Home</router-link> |
+                <router-link to="/about">About</router-link>
+            </nav>
+
+            <router-view />
+        </v-main>
+    </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+    import { defineComponent } from "vue";
 
-export default defineComponent({
-  name: 'App',
+    export default defineComponent({
+        name: "App",
 
-  data () {
-    return {
-      //
-    }
-  },
-})
+        data() {
+            return {
+                //
+            };
+        },
+    });
 </script>
+
+<style lang="scss">
+    nav {
+        padding: 30px;
+        text-align: center;
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+                color: #42b983;
+            }
+        }
+    }
+</style>
